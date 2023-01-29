f4fa235c409e5b3e2489e4a264f5445be0bb4ceb
ziggekatten <mats@brorson.se>
2023-01-29 21:19:48 +0100

    cleaned upp comments in hwdefs

--- code/firmware/src/hwdefs.h
+++ code/firmware/src/hwdefs.h
@@ -6,12 +6,13 @@
 * Memory map is quite simple: ROM at the base (1 MB), followedd by 14 MB SRAM, leaving top 1 MB for I/O space
 */
 
-//ROM start address
+/* ROM start address */
 #define ROM_BASE        0x00000000
 #define ROM_SIZE        (ROM_BASE + 0x000FFFFF) // 1 MB of ROM (2 * 512k)
-//RAM start address
+/* RAM start address */
 #define RAM_BASE        0x00100000
 #define RAM_SIZE        (RAM_BASE + 0x000FFFFF) 
 
-// Stack pointer at end of onboard 1 MB (must be at even address. Remember, sp is decreased BEFORE adding data to stack)
+/* Stack pointer at end of onboard 1 MB (must be at even address. 
+* Remember, sp is decreased BEFORE adding data to stack) */
 #define STACK_POINTER   0x00200000
