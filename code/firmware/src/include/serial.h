4e07762be8ff5d9cb9bf146c9bbe16eb36fd0e29
ziggekatten <mats@brorson.se>
2023-01-29 17:26:49 +0100

    Structuring of header file location

---
+++ code/firmware/src/include/serial.h
@@ -0,0 +1,14 @@
+/*
+* @Author: Mats Brorson
+* @Date: 2023-01-27
+* 
+* @Description: Functions to initialize and start serial communications
+*/
+#ifndef _MB68K_SERIAL_H
+#define _MB68K_SERIAL_H
+
+/*prototype for initializing Serial commmunication function in DUART */
+void Init_Serial(void);
+
+
+#endif
